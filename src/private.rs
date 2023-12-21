2924d2d66083dec3d7d47a38aebc92419648c675
codecraft26 <gaman0221@gmial.com>
2023-12-21 14:14:46 +0530

    Add MongoDB implementation and private constants

---
+++ src/private.rs
@@ -0,0 +1,3 @@
+pub const JWT_SECRET: &'static str = "djdj";
+pub const REFRESH_JWT_SECRET: &'static str = "dkfnrfufrn";
+pub(crate) const URL_DB: &str = "mongodb+srv://aman:aman@cluster0.suqkggz.mongodb.net/?retryWrites=true&w=majority";
